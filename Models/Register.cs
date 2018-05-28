ac85104722c9115e4cc86af7b1dafc5e2982497d
KamvaLihle <38459457+KamvaLihle@users.noreply.github.com>
2018-05-28 10:23:59 +0200

    flowerPop Web Application

---
+++ Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Register
+    {
+        public string emailID
+        {
+            get;
+            set;
+        }
+
+        public string password
+        {
+            get;
+            set;
+        }
+    }
+}
